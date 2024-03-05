b222ae096c99e87eee9b311ff18d147ee51962f9
Vimalnath Nambiar <vnathnambiar@hotmail.com>
2024-03-05 12:08:08 +0800

    fix: removed unused dependencies
    removed babel, eslint-config-cheminfo, jest; and updated variable name for roundDecimalPlace function

--- src/roundDecimalPlace.js
+++ src/roundDecimalPlace.js
@@ -1,10 +1,10 @@
 /**
  * Round values to a specific decimal place.
- * @param {number} number The number you want to round.
+ * @param {number} toRoundValue The number value you want to round.
  * @param {number} decimalPlace The number of decimal places you would like to round to.
  * @returns {Promise<number>} A promise that resolves with the rounded value to the specified number of decimal places.
  */
-export async function roundDecimalPlace(number, decimalPlace) {
+export async function roundDecimalPlace(toRoundValue, decimalPlace) {
   const factor = 10 ** decimalPlace;
-  return Math.round(number * factor) / factor;
+  return Math.round(toRoundValue * factor) / factor;
 }
